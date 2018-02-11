a5e85943dc9bb028bf7e61289556c2dd297dcf00
James Bratt <hello@jamesbratt.co.uk>
2018-02-11 18:42:57 +0000

    Initial integration of react dnd

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
-import Textinput from './form-components/text-input'
-import logo from './logo.svg';
 import './App.css';
+import Container from './Container'
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.fields = [];
-    this.state = {
-      fields: this.fields
-    };
-  }
-
-  addField = () => {
-    this.fields.push(this.fields.length + 1);
-    this.setState({
-      fields: this.fields
-    });
-  }
-
-  removeField = (key) => {
-    this.fields.splice(key, 1);
-    this.setState({
-      fields: this.fields
-    });
-  }
-
-  render() {
-    const props = {
-      onRemove: this.removeField,
-    }
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To add more fields, click "Add New Field".
-        </p>
-        <button onClick={this.addField} className='btn-add'>Add New Field</button>
-        <div id='form'>
-          {this.state.fields.map((field, i) =>
-            <Textinput key={i} id={i} placeholder={'this is id ' + i} {...props} />
-          )}
-        </div>
-      </div>
-    );
-  }
+export default class SortableSimple extends Component {
+	render() {
+		return (
+			<div>
+				<Container />
+			</div>
+		)
+	}
 }
-
-export default App;
--- src/form-components/text-input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Textinput(props) {
-    return (
-        <input onClick={() => props.onRemove(props.id)} type='text' placeholder={props.placeholder} />
-    );
-}
-
-export default Textinput;
